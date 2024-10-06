e9e135917b5f2e232573091929e024c957e13005
Mustan Ali <74346675+mustan-ali@users.noreply.github.com>
2024-10-06 16:23:46 +0500

    update cors configuration to allow CLIENT_URL

--- backend/server.js
+++ backend/server.js
@@ -10,7 +10,7 @@ const userRoutes = require('./routes/user');
 
 
 // Middleware
-app.use(cors());
+app.use(cors({ origin: process.env.CLIENT_URL, credentials: true }));
 app.use(express.json());
 app.use(cookieParser());
 app.use('/api/auth', authRoutes);
