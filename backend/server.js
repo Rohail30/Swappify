d27f7ba75be505a8992117cc738fa67e40a8ad12
Mustan Ali <74346675+mustan-ali@users.noreply.github.com>
2024-10-06 11:55:31 +0500

    Add cookie-parser and cors packages for handling cookies and enabling CORS

--- backend/server.js
+++ backend/server.js
@@ -2,13 +2,17 @@ const express = require('express');
 const app = express();
 require('dotenv').config();
 const PORT = process.env.PORT || 5000;
+const cors = require('cors');
+const cookieParser = require('cookie-parser');
 const connectDB = require('./config/database');
 const authRoutes = require('./routes/auth');
 const userRoutes = require('./routes/user');
 
 
 // Middleware
+app.use(cors());
 app.use(express.json());
+app.use(cookieParser());
 app.use('/api/auth', authRoutes);
 app.use('/api/users', userRoutes);
 
