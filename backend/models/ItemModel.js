1d1b807baeee45275d2307aca95303e2d424f56c
Mustan Ali <74346675+mustan-ali@users.noreply.github.com>
2024-10-07 14:58:18 +0500

    add ItemModel schema for items in the backend

---
+++ backend/models/ItemModel.js
@@ -0,0 +1,60 @@
+const mongoose = require("mongoose");
+
+const ItemSchema = new mongoose.Schema(
+    {
+        name: {
+            type: String,
+            required: true,
+        },
+        description: {
+            type: String,
+            required: true,
+        },
+        owner: {
+            type: mongoose.Schema.Types.ObjectId,
+            ref: "User",
+            required: true,
+        },
+        image: {
+            type: String,
+            required: true,
+        },
+        condition: {
+            type: String,
+            enum: ["new", "used"],
+            required: true,
+        },
+        category: {
+            type: String,
+            enum: [
+                'electronics', 'furniture', 'clothing & accessories', 'books & media', 'home & garden', 'sports & outdoors', 'toys & games', 'tools & hardware', 'automotive', 'office supplies', 'collectibles & antiques', 'other'
+            ],
+            required: true,
+        },
+        location: {
+            type: String,
+            required: true,
+        },
+        price: {
+            min: {
+                type: Number,
+                required: true,
+                min: 0,
+            },
+            max: {
+                type: Number,
+                required: true,
+                min: 0,
+            },
+        },
+        status: {
+            type: String,
+            enum: ["available", "traded", "in trade"],
+            required: true,
+            default: "available",
+        },
+    },
+    { timestamps: true }
+);
+
+module.exports = mongoose.model("Item", ItemSchema);
