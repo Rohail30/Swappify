d88789505891a7d6278ee32e336ca997be33603f
Mustan Ali <74346675+mustan-ali@users.noreply.github.com>
2024-10-11 10:48:15 +0500

    Add static file serving middleware

--- backend/server.js
+++ backend/server.js
@@ -3,14 +3,15 @@ const app = express();
 require('dotenv').config();
 const PORT = process.env.PORT || 5000;
 const cors = require('cors');
+const path = require('path');
 const cookieParser = require('cookie-parser');
 const connectDB = require('./config/database');
 const authRoutes = require('./routes/auth');
 const userRoutes = require('./routes/user');
 
 
-// Middleware
 app.use(cors({ origin: process.env.CLIENT_URL, credentials: true }));
+app.use(express.static(path.join(__dirname, 'public')));
 app.use(express.json());
 app.use(cookieParser());
 app.use('/api/auth', authRoutes);
