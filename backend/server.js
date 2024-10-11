c14d37a913b0557440abfa2e9e7488bed5e0f4f2
Mustan Ali <74346675+mustan-ali@users.noreply.github.com>
2024-10-11 12:21:22 +0500

    Refactor .gitignore and server.js files

--- backend/server.js
+++ backend/server.js
@@ -8,14 +8,16 @@ const cookieParser = require('cookie-parser');
 const connectDB = require('./config/database');
 const authRoutes = require('./routes/auth');
 const userRoutes = require('./routes/user');
+const itemRoutes = require('./routes/item');
 
 
 app.use(cors({ origin: process.env.CLIENT_URL, credentials: true }));
-app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static(path.join(__dirname, './public')));
 app.use(express.json());
 app.use(cookieParser());
 app.use('/api/auth', authRoutes);
 app.use('/api/users', userRoutes);
+app.use('/api/items', itemRoutes);
 
 
 app.listen(PORT, () => {
