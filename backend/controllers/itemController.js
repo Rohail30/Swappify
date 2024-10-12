cf485b76b7e54046a407f20259a0f922dc7cb86b
Mustan Ali <74346675+mustan-ali@users.noreply.github.com>
2024-10-12 11:12:29 +0500

    add updateItem, deleteItem, getAllItem, getItem, getItemsByUser functionality

--- backend/controllers/itemController.js
+++ backend/controllers/itemController.js
@@ -1,5 +1,6 @@
 const Item = require("../models/ItemModel");
-const upload = require("../config/multer");
+const fs = require("fs");
+const path = require("path");
 
 
 // @desc    Add a new item
@@ -8,47 +9,172 @@ const upload = require("../config/multer");
 
 const addItem = async (req, res) => {
 
-    upload(req, res, async (err) => {
+    const { name, description, owner, condition, category, location, priceMin, priceMax } = req.body;
 
-        const { name, description, owner, condition, category, location, priceMin, priceMax } = req.body;
+    if (!name || !description || !owner || !condition || !category || !location || !priceMin || !priceMax) {
+        return res.status(400).json({ error: true, message: "Please enter all the required fields" });
+    }
 
-        if (!name || !description || !owner || !condition || !category || !location || !priceMin || !priceMax) {
-            return res.status(400).json({ error: true, message: "Please enter all the required fields" });
-        }
+    if (priceMin < 0 || priceMax < 0) {
+        return res.status(400).json({ error: true, message: "Price cannot be negative" });
+    }
 
-        if (priceMin < 0 || priceMax < 0) {
-            return res.status(400).json({ error: true, message: "Price cannot be negative" });
-        }
-        if (err) {
-            return res.status(400).json({ error: true, message: err.message });
-        }
+    if (!req.file) {
+        return res.status(400).json({ error: true, message: "Image is required" });
+    }
 
-        if (!req.file) {
-            return res.status(400).json({ error: true, message: "Image is required" });
-        }
+    try {
+        const newItem = await Item.create({
+            name,
+            description,
+            owner,
+            image: `/images/${req.file.filename}`,
+            condition,
+            category,
+            location,
+            price: {
+                min: priceMin,
+                max: priceMax,
+            },
+        });
 
-        try {
-            const newItem = await Item.create({
-                name,
-                description,
-                owner,
-                image: `/images/${req.file.filename}`,
-                condition,
-                category,
-                location,
-                price: {
-                    min: priceMin,
-                    max: priceMax,
-                },
-            });
+        return res.status(200).json({ error: false, message: "Item added successfully", item: newItem });
 
-            return res.status(200).json({ error: false, message: "Item added successfully", item: newItem });
-
-        } catch (error) {
-            return res.status(500).json({ error: true, message: error.message });
-        }
-    });
+    } catch (error) {
+        return res.status(500).json({ error: true, message: error.message });
+    }
 };
 
 
-module.exports = { addItem };
+
+// @desc    Update an item
+// @route   POST /api/items/update/:id
+// @access  Private
+
+const updateItem = async (req, res) => {
+
+    const { name, description, owner, condition, category, location, priceMin, priceMax } = req.body;
+
+    if (!name || !description || !owner || !condition || !category || !location || !priceMin || !priceMax) {
+        return res.status(400).json({ error: true, message: "Please enter all the required fields" });
+    }
+
+    if (priceMin < 0 || priceMax < 0) {
+        return res.status(400).json({ error: true, message: "Price cannot be negative" });
+    }
+
+    try {
+        const item = await Item.findById(req.params.id);
+
+        if (!item) {
+            return res.status(404).json({ error: true, message: "Item not found" });
+        }
+
+        if (req.file) {
+            item.image = `/images/${req.file.filename}`;
+        }
+
+        const updatedItem = await Item.findByIdAndUpdate(req.params.id, {
+            name,
+            description,
+            owner,
+            condition,
+            category,
+            location,
+            price: {
+                min: priceMin,
+                max: priceMax,
+            },
+            image: item.image,
+        }, { new: true });
+
+        return res.status(200).json({ error: false, message: "Item updated successfully", updatedItem });
+
+    } catch (error) {
+        return res.status(500).json({ error: true, message: error.message });
+    }
+};
+
+
+
+// @desc    Delete an item
+// @route   DELETE /api/items/delete/:id
+// @access  Private
+
+const deleteItem = async (req, res) => {
+
+    try {
+        const item = await Item.findById(req.params.id);
+
+        if (!item) {
+            return res.status(404).json({ error: true, message: "Item not found" });
+        }
+
+        await Item.findByIdAndDelete(req.params.id);
+
+        fs.unlinkSync(path.join(__dirname, `../public${item.image}`));
+
+        return res.status(200).json({ error: false, message: "Item deleted successfully" });
+
+    } catch (error) {
+        return res.status(500).json({ error: true, message: error.message });
+    }
+}
+
+
+// @desc    Get all items
+// @route   GET /api/items
+// @access  Public
+
+const getAllItems = async (req, res) => {
+
+    try {
+        const items = await Item.find();
+
+        return res.status(200).json({ error: false, items });
+
+    } catch (error) {
+        return res.status(500).json({ error: true, message: error.message });
+    }
+}
+
+
+// @desc    Get a single item
+// @route   GET /api/items/:id
+// @access  Public
+
+const getItem = async (req, res) => {
+
+    try {
+        const item = await Item.findById(req.params.id)
+
+        if (!item) {
+            return res.status(404).json({ error: true, message: "Item not found" })
+        }
+
+        return res.status(200).json({ error: false, item });
+
+    } catch (error) {
+        return res.status(500).json({ error: true, message: error.message })
+    }
+}
+
+
+// @desc    Get All items by particular user
+// @route   GET /api/items/user/:id
+// @access  Public
+
+const getItemsByUser = async (req, res) => {
+
+    try {
+        const items = await Item.find({ owner: req.params.id });
+
+        return res.status(200).json({ error: false, items });
+
+    } catch (error) {
+        return res.status(500).json({ error: true, message: error.message });
+    }
+}
+
+
+module.exports = { addItem, updateItem, deleteItem, getAllItems, getItem, getItemsByUser };
--- backend/routes/item.js
+++ backend/routes/item.js
@@ -1,11 +1,31 @@
 const express = require('express');
 const router = express.Router();
 
-const { addItem } = require('../controllers/itemController');
+const { addItem, updateItem, deleteItem, getAllItems, getItem, getItemsByUser } = require('../controllers/itemController');
+const upload = require('../config/multer');
+
+
+// @desc    Get all items
+router.get('/', getAllItems);
 
 
 // @desc    Add a new item
-router.post('/add', addItem);
+router.post('/add', upload, addItem);
 
+
+// @desc    Update an item
+router.put('/update/:id', upload, updateItem);
+
+
+// @desc    Delete an item
+router.delete('/delete/:id', deleteItem);
+
+
+// @desc    Get a single item
+router.get('/:id', getItem);
+
+
+// @desc    Get All items by particular user
+router.get('/user/:id', getItemsByUser);
 
 module.exports = router;
