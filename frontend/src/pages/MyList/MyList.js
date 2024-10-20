efd26ec923ae9b475a288a1e8af392c77f3ad78a
Mustan Ali <74346675+mustan-ali@users.noreply.github.com>
2024-10-20 15:01:04 +0500

    Refactor MyList to display user-specific items

---
+++ frontend/src/pages/MyList/MyList.js
@@ -0,0 +1,90 @@
+import "./MyList.css";
+import { Link } from "react-router-dom";
+import { useEffect, useState, useContext } from 'react';
+import { MdDelete } from "react-icons/md";
+import { MdEdit } from "react-icons/md";
+import { AuthContext } from "../../context/AuthContext";
+import apiRequest from "../../config/apiRequest";
+
+
+const MyList = () => {
+
+    const { currentUser } = useContext(AuthContext);
+    const [items, setItems] = useState([]);
+
+    useEffect(() => {
+
+        const fetchItems = async () => {
+            try {
+                const res = await apiRequest.get(`api/items/user/${currentUser._id}`)
+                setItems(res.data.items);
+            }
+            catch (error) {
+                console.log(error);
+            }
+        }
+        fetchItems();
+
+    }, [currentUser._id]);
+
+    const handleDelete = async (id) => {
+        try {
+            await apiRequest.delete(`api/items/delete/${id}`);
+            setItems(items.filter((item) => item._id !== id));
+        }
+        catch (error) {
+            console.log(error);
+        }
+    }
+
+    return (
+        <div className="mylist">
+            <div className="container">
+                <div className="info">
+                    <h1>My List</h1>
+                </div>
+                <Link to="/add-item">
+                    <button>Create New Post</button>
+                </Link>
+            </div>
+
+            <div className="cards">
+                <div className="card-container">
+                    {items.length === 0 ? (
+                        <p className="empty-text">No items yet! Add one now.</p>
+                    ) : (
+                        items.map((item) => (
+                            <div className="mylistcard">
+                                <div className="image-container">
+                                    <div className="image">
+                                        <img src={`http://localhost:5000${item.image}`} alt="Item" />
+                                    </div>
+                                </div>
+                                <div className="line-container">
+                                    <div className="line"></div>
+                                </div>
+                                <div className="title"><h1>{item.name}</h1></div>
+                                <div className="status"><h2>{item.status}</h2></div>
+                                <div className="buttons">
+                                    <div className="button1">
+                                        <Link to="/my-list-detail">
+                                            <div className="view">View details</div>
+                                        </Link>
+                                    </div>
+                                    <div className="button2">
+                                        <Link to={`/edit-item/${item._id}`}><div className="edit"><MdEdit /></div></Link>
+                                        {/* <div className="delete"><MdDelete /></div> */}
+                                        <div className="delete" onClick={() => handleDelete(item._id)}><MdDelete /></div>
+                                    </div>
+                                </div>
+                            </div>
+                        ))
+                    )
+                    }
+                </div>
+            </div>
+        </div>
+    );
+}
+
+export default MyList;
--- frontend/src/pages/Profile/Profile.js
+++ frontend/src/pages/Profile/Profile.js
@@ -2,6 +2,7 @@ import "./profile.css";
 import { useContext } from "react";
 import { Link } from "react-router-dom";
 import { AuthContext } from "../../context/AuthContext";
+import MyList from "../MyList/MyList";
 
 const ProfilePage = () => {
 
@@ -9,21 +10,25 @@ const ProfilePage = () => {
 
     return (
         <div className="profilePage">
-            <div className="info">
-                <h1>User Information</h1>
-                <span>
-                    Full Name: <b>{currentUser.name}</b>
-                </span>
-                <span>
-                    E-Mail: <b>{currentUser.email}</b>
-                </span>
-                <span>
-                    Mobile: <b>{currentUser.mobile}</b>
-                </span>
+            <div className="container">
+                <div className="info">
+                    <h1 id="user-info-heading">User Information</h1>
+                    <span>
+                        Full Name: <b>{currentUser.name}</b>
+                    </span>
+                    <span>
+                        E-Mail: <b>{currentUser.email}</b>
+                    </span>
+                    <span>
+                        Mobile: <b>{currentUser.mobile}</b>
+                    </span>
+
+                </div>
                 <Link to="/update-profile">
                     <button>Update Profile</button>
                 </Link>
             </div>
+            <MyList />
         </div>
     );
 }
