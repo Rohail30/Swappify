5c75ddb7b932b7a64fe907c6e6c0d989740d8101
Rohail Rathore <RohailRathore10@hotmail.com>
2024-11-19 01:16:45 +0500

    UI Improved: Ttile slice added

--- frontend/src/pages/EditItem/EditItem.js
+++ frontend/src/pages/EditItem/EditItem.js
@@ -1,155 +1,204 @@
-import "./editItem.css";
-import { useState, useContext, useEffect } from "react";
-import apiRequest from "../../config/apiRequest";
-import { AuthContext } from "../../context/AuthContext";
-import { useParams } from "react-router-dom"
-import { useNavigate } from "react-router-dom";
-
+import './editItem.css';
+import { useState, useContext, useEffect } from 'react';
+import apiRequest from '../../config/apiRequest';
+import { AuthContext } from '../../context/AuthContext';
+import { useParams } from 'react-router-dom';
+import { useNavigate } from 'react-router-dom';
 
 const EditItem = () => {
+  const { currentUser } = useContext(AuthContext);
+  const navigate = useNavigate();
 
-    const { currentUser } = useContext(AuthContext);
-    const navigate = useNavigate();
+  const [error, setError] = useState(null);
+  const [itemData, setItemData] = useState({
+    name: '',
+    description: '',
+    image: undefined,
+    condition: '',
+    category: '',
+    location: '',
+    priceMin: 0,
+    priceMax: 0,
+    owner: currentUser._id,
+  });
 
-    const [error, setError] = useState(null);
-    const [itemData, setItemData] = useState({
-        name: "",
-        description: "",
-        image: undefined,
-        condition: "",
-        category: "",
-        location: "",
-        priceMin: 0,
-        priceMax: 0,
-        owner: currentUser._id,
-    });
+  const [imagePreview, setImagePreview] = useState(null);
+  const { id } = useParams();
 
-    const [imagePreview, setImagePreview] = useState(null);
-    const { id } = useParams();
+  useEffect(() => {
+    const fetchItem = async () => {
+      try {
+        const res = await apiRequest.get(`/api/items/${id}`);
 
-    useEffect(() => {
-        const fetchItem = async () => {
-            try {
-                const res = await apiRequest.get(`/api/items/${id}`);
-
-                setItemData({
-                    name: res.data.item.name,
-                    description: res.data.item.description,
-                    image: res.data.item.image,
-                    condition: res.data.item.condition,
-                    category: res.data.item.category,
-                    location: res.data.item.location,
-                    priceMin: res.data.item.price.min,
-                    priceMax: res.data.item.price.max,
-                    owner: res.data.item.owner,
-                });
-                setImagePreview(`http://localhost:5000${res.data.item.image}`);
-            } catch (error) {
-                console.log(error);
-            }
-        }
-        fetchItem();
-
-    }, [id]);
-
-
-    const handleChange = (e) => {
-        if (e.target.name === "image") {
-            const file = e.target.files[0];
-            setItemData({ ...itemData, image: file });
-            setImagePreview(URL.createObjectURL(file));
-            setError(null);
-        } else {
-            setItemData({ ...itemData, [e.target.name]: e.target.value });
-            setError(null);
-        }
+        setItemData({
+          name: res.data.item.name,
+          description: res.data.item.description,
+          image: res.data.item.image,
+          condition: res.data.item.condition,
+          category: res.data.item.category,
+          location: res.data.item.location,
+          priceMin: res.data.item.price.min,
+          priceMax: res.data.item.price.max,
+          owner: res.data.item.owner,
+        });
+        setImagePreview(`http://localhost:5000${res.data.item.image}`);
+      } catch (error) {
+        console.log(error);
+      }
     };
+    fetchItem();
+  }, [id]);
 
+  const handleChange = (e) => {
+    if (e.target.name === 'image') {
+      const file = e.target.files[0];
+      setItemData({ ...itemData, image: file });
+      setImagePreview(URL.createObjectURL(file));
+      setError(null);
+    } else {
+      setItemData({ ...itemData, [e.target.name]: e.target.value });
+      setError(null);
+    }
+  };
 
-    const handleSubmit = async (e) => {
-        e.preventDefault();
+  const handleSubmit = async (e) => {
+    e.preventDefault();
 
-        const formData = new FormData();
+    const formData = new FormData();
 
-        formData.append("name", itemData.name);
-        formData.append("description", itemData.description);
-        formData.append("condition", itemData.condition);
-        formData.append("category", itemData.category);
-        formData.append("location", itemData.location);
-        formData.append("priceMin", itemData.priceMin);
-        formData.append("priceMax", itemData.priceMax);
-        formData.append("owner", itemData.owner);
-        formData.append("image", itemData.image);
+    formData.append('name', itemData.name);
+    formData.append('description', itemData.description);
+    formData.append('condition', itemData.condition);
+    formData.append('category', itemData.category);
+    formData.append('location', itemData.location);
+    formData.append('priceMin', itemData.priceMin);
+    formData.append('priceMax', itemData.priceMax);
+    formData.append('owner', itemData.owner);
+    formData.append('image', itemData.image);
 
-        try {
-            await apiRequest.put(`/api/items/update/${id}`, formData, {
-                headers: {
-                    "Content-Type": "multipart/form-data",
-                },
-            });
-            navigate("/profile");
-        } catch (error) {
-            setError(error.response?.data?.message || "An error occurred. Please try again.");
-        }
-    };
+    try {
+      await apiRequest.put(`/api/items/update/${id}`, formData, {
+        headers: {
+          'Content-Type': 'multipart/form-data',
+        },
+      });
+      navigate('/profile');
+    } catch (error) {
+      setError(
+        error.response?.data?.message || 'An error occurred. Please try again.'
+      );
+    }
+  };
 
+  return (
+    <div className="editItemPage">
+      <div className="formContainer">
+        <form onSubmit={handleSubmit}>
+          <h1>Edit Item</h1>
 
-    return (
-        <div className="editItemPage">
-            <div className="formContainer">
-                <form onSubmit={handleSubmit}>
-                    <h1>Edit Item</h1>
+          <input
+            type="text"
+            name="name"
+            value={itemData.name}
+            placeholder="Item Name"
+            onChange={handleChange}
+            required
+          />
 
-                    <input type="text" name="name" value={itemData.name} placeholder="Item Name" onChange={handleChange} required />
+          <textarea
+            name="description"
+            value={itemData.description}
+            placeholder="Item Description"
+            onChange={handleChange}
+            required
+          />
 
-                    <textarea name="description" value={itemData.description} placeholder="Item Description" onChange={handleChange} required />
+          <select
+            name="condition"
+            value={itemData.condition}
+            onChange={handleChange}
+            required
+          >
+            <option value="" disabled>
+              Condition
+            </option>
+            <option value="new">New</option>
+            <option value="used">Used</option>
+          </select>
 
+          <select
+            name="category"
+            value={itemData.category}
+            onChange={handleChange}
+            required
+          >
+            <option value="" disabled>
+              Category
+            </option>
+            <option value="electronics">Electronics</option>
+            <option value="furniture">Furniture</option>
+            <option value="clothing & accessories">
+              Clothing & Accessories
+            </option>
+            <option value="books & media">Books & Media</option>
+            <option value="home & garden">Home & Garden</option>
+            <option value="sports & outdoors">Sports & Outdoors</option>
+            <option value="toys & games">Toys & Games</option>
+            <option value="tools & hardware">Tools & Hardware</option>
+            <option value="automotive">Automotive</option>
+            <option value="office supplies">Office Supplies</option>
+            <option value="collectibles & antiques">
+              Collectibles & Antiques
+            </option>
+            <option value="other">Other</option>
+          </select>
 
-                    <select name="condition" value={itemData.condition} onChange={handleChange} required>
-                        <option value="" disabled>Condition</option>
-                        <option value="new">New</option>
-                        <option value="used">Used</option>
-                    </select>
+          <input
+            type="text"
+            name="location"
+            value={itemData.location}
+            placeholder="Location"
+            onChange={handleChange}
+            required
+          />
 
-                    <select name="category" value={itemData.category} onChange={handleChange} required>
-                        <option value="" disabled>Category</option>
-                        <option value="electronics">Electronics</option>
-                        <option value="furniture">Furniture</option>
-                        <option value="clothing & accessories">Clothing & Accessories</option>
-                        <option value="books & media">Books & Media</option>
-                        <option value="home & garden">Home & Garden</option>
-                        <option value="sports & outdoors">Sports & Outdoors</option>
-                        <option value="toys & games">Toys & Games</option>
-                        <option value="tools & hardware">Tools & Hardware</option>
-                        <option value="automotive">Automotive</option>
-                        <option value="office supplies">Office Supplies</option>
-                        <option value="collectibles & antiques">Collectibles & Antiques</option>
-                        <option value="other">Other</option>
-                    </select>
+          <input
+            type="number"
+            name="priceMin"
+            value={itemData.priceMin}
+            placeholder="Minimum Price"
+            onChange={handleChange}
+            required
+          />
 
-                    <input type="text" name="location" value={itemData.location} placeholder="Location" onChange={handleChange} required />
+          <input
+            type="number"
+            name="priceMax"
+            value={itemData.priceMax}
+            placeholder="Maximum Price"
+            onChange={handleChange}
+            required
+          />
 
-                    <input type="number" name="priceMin" value={itemData.priceMin} placeholder="Minimum Price" onChange={handleChange} required />
+          <input type="file" name="image" onChange={handleChange} />
 
-                    <input type="number" name="priceMax" value={itemData.priceMax} placeholder="Maximum Price" onChange={handleChange} required />
+          {error && <span className="error-message">{error}</span>}
 
-                    <input type="file" name="image" onChange={handleChange} />
+          <button type="submit">Edit Item</button>
+        </form>
 
-                    {error && <span className="error-message">{error}</span>}
-
-                    <button type="submit">Edit Item</button>
-
-                </form>
-
-                <div className="imagePreview">
-                    {imagePreview && (
-                        <img src={imagePreview} alt="Item Preview" style={{ width: "300px", height: "auto" }} />
-                    )}
-                </div>
-
-            </div>
+        <div className="imagePreview">
+          {imagePreview && (
+            <img
+              src={imagePreview}
+              alt="Item Preview"
+              style={{ width: '300px', height: 'auto' }}
+            />
+          )}
         </div>
-    );
+      </div>
+    </div>
+  );
 };
 
 export default EditItem;
--- frontend/src/pages/Wishlist/Wishlist.js
+++ frontend/src/pages/Wishlist/Wishlist.js
@@ -1,94 +1,107 @@
-import "./Wishlist.css";
-import { MdDelete } from "react-icons/md";
-import { Link } from "react-router-dom";
-import apiRequest from "../../config/apiRequest";
+import './Wishlist.css';
+import { MdDelete } from 'react-icons/md';
+import { Link } from 'react-router-dom';
+import apiRequest from '../../config/apiRequest';
 import { useEffect, useState } from 'react';
 
 const Wishlist = () => {
-    const [items, setItems] = useState([]);
+  const [items, setItems] = useState([]);
 
-    useEffect(() => {
-        const fetchItems = async () => {
-            try {
-                const res = await apiRequest.get(`/api/wishlist`);
-                const itemIds = res.data.wishlist.items.map(item => item.itemId);
+  useEffect(() => {
+    const fetchItems = async () => {
+      try {
+        const res = await apiRequest.get(`/api/wishlist`);
+        const itemIds = res.data.wishlist.items.map((item) => item.itemId);
 
-                const itemDetails = await Promise.all(
-                    itemIds.map(async (itemId) => {
-                        const itemData = await apiRequest.get(`/api/items/${itemId}`);
-                        return itemData.data.item;
-                    })
-                );
+        const itemDetails = await Promise.all(
+          itemIds.map(async (itemId) => {
+            const itemData = await apiRequest.get(`/api/items/${itemId}`);
+            return itemData.data.item;
+          })
+        );
 
-                setItems(itemDetails);
-            } catch (error) {
-                console.log("Error fetching items:", error);
-            }
-        };
+        setItems(itemDetails);
+      } catch (error) {
+        console.log('Error fetching items:', error);
+      }
+    };
 
-        fetchItems();
-    }, []);
+    fetchItems();
+  }, []);
 
-    const handleRemoveFromWishlist = async (id) => {
-
-        const confirmDelete = window.confirm("Do you really want to delete this item from your wishlist?");
-        if (confirmDelete) {
-            try {
-                await apiRequest.delete(`/api/wishlist/${id}`);
-                setItems(items.filter((item) => item._id !== id));
-            }
-            catch (error) {
-                console.log(error);
-            }
-        }
-    }
-
-    return (
-        <div className="wishlist">
-            <div className="heading">
-                <h1>Wishlist</h1>
-            </div>
-            <div className="card-container">
-                {items.length === 0 ? (
-                    <p className="empty-text">No items yet! Add one now.</p>
-                ) : (
-                    items.map((item) => (
-                        <div className="wishlistcard" key={item._id}>
-                            <div className="image-container">
-                                <div className="image">
-                                    <img src={`http://localhost:5000${item.image}`} alt="Item" />
-                                </div>
-                            </div>
-                            <div className="line-container">
-                                <div className="line"></div>
-                            </div>
-                            <div className="details">
-                                <div className="title"><h1>{item.name}</h1></div>
-                                <div className="status"><h2>{item.status}</h2></div>
-                                <div className="price">
-                                    <h2>{`Est. Value: ${item.price.min} - ${item.price.max}`}</h2>
-                                </div>
-                                <div className="location">
-                                    <h3>{item.location}</h3>
-                                    <h3>{item.category}</h3>
-                                </div>
-                            </div>
-                            <div className="buttons">
-                                <div className="button1">
-                                    <Link to={`/detail-page/${item._id}`}>
-                                        <div className="view">View details</div>
-                                    </Link>
-                                </div>
-                                <div className="button2">
-                                    <div className="delete" onClick={() => handleRemoveFromWishlist(item._id)}><MdDelete className="custom-icon" /></div>
-                                </div>
-                            </div>
-                        </div>
-                    ))
-                )}
-            </div>
-        </div>
+  const handleRemoveFromWishlist = async (id) => {
+    const confirmDelete = window.confirm(
+      'Do you really want to delete this item from your wishlist?'
     );
-}
+    if (confirmDelete) {
+      try {
+        await apiRequest.delete(`/api/wishlist/${id}`);
+        setItems(items.filter((item) => item._id !== id));
+      } catch (error) {
+        console.log(error);
+      }
+    }
+  };
+
+  return (
+    <div className="wishlist">
+      <div className="heading">
+        <h1>Wishlist</h1>
+      </div>
+      <div className="card-container">
+        {items.length === 0 ? (
+          <p className="empty-text">No items yet! Add one now.</p>
+        ) : (
+          items.map((item) => (
+            <div className="wishlistcard" key={item._id}>
+              <div className="image-container">
+                <div className="image">
+                  <img src={`http://localhost:5000${item.image}`} alt="Item" />
+                </div>
+              </div>
+              <div className="line-container">
+                <div className="line"></div>
+              </div>
+              <div className="details">
+                <div className="title">
+                  <h1>
+                    {item.name.length > 16
+                      ? item.name.slice(0, 14) + '...'
+                      : item.name}
+                  </h1>
+                </div>
+                <div className="status">
+                  <h2>{item.status}</h2>
+                </div>
+                <div className="price">
+                  <h2>{`Est. Value: ${item.price.min} - ${item.price.max}`}</h2>
+                </div>
+                <div className="location">
+                  <h3>{item.location}</h3>
+                  <h3>{item.category}</h3>
+                </div>
+              </div>
+              <div className="buttons">
+                <div className="button1">
+                  <Link to={`/detail-page/${item._id}`}>
+                    <div className="view">View details</div>
+                  </Link>
+                </div>
+                <div className="button2">
+                  <div
+                    className="delete"
+                    onClick={() => handleRemoveFromWishlist(item._id)}
+                  >
+                    <MdDelete className="custom-icon" />
+                  </div>
+                </div>
+              </div>
+            </div>
+          ))
+        )}
+      </div>
+    </div>
+  );
+};
 
 export default Wishlist;
